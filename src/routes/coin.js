7a34728071f3b8668cac6afd135728125ee5fce4
damebeus <damiebeus@gmail.com>
2022-08-16 02:40:16 -0300

    tolower activo

--- src/routes/coin.js
+++ src/routes/coin.js
@@ -5,18 +5,19 @@ const { FavCoin } = require("../db");
 
 router.get("/crypto/:ticker", async (req, res) => {
   const { ticker } = req.params;
+  const tickerLowerCase = ticker.toLowerCase();
+
   try {
     let apiUrl = await axios.get(
-      `https://api.cryptapi.io/${ticker}/info/?prices=1`
+      `https://api.cryptapi.io/${tickerLowerCase}/info/?prices=1`
     );
 
     const apiCoin = {
       logo: apiUrl.data.logo,
       coin: apiUrl.data.coin,
-      ticker: apiUrl.data.ticker,
-      prices: apiUrl.data.prices,
+      ticker: apiUrl.data.ticker.toUpperCase(),
+      prices: parseFloat(ethereum.data.prices.USD).toFixed(2),
     };
-
     return res.send(apiCoin);
   } catch (error) {
     console.log(error);
@@ -35,30 +36,30 @@ router.get("/allCrypto", async (req, res) => {
 
     const apiCoin = [
       {
-        logo : bitcoin.data.logo,
-        coin : bitcoin.data.coin,
-        ticker : bitcoin.data.ticker,
-        prices: parseFloat(bitcoin.data.prices.USD).toFixed(2)
+        logo: bitcoin.data.logo,
+        coin: bitcoin.data.coin,
+        ticker: bitcoin.data.ticker,
+        prices: parseFloat(bitcoin.data.prices.USD).toFixed(2),
       },
       {
-        logo : ethereum.data.logo,
-        coin : ethereum.data.coin,
-        ticker : ethereum.data.ticker,
-        prices: parseFloat(ethereum.data.prices.USD).toFixed(2)
+        logo: ethereum.data.logo,
+        coin: ethereum.data.coin,
+        ticker: ethereum.data.ticker,
+        prices: parseFloat(ethereum.data.prices.USD).toFixed(2),
       },
       {
-        logo : litecoin.data.logo,
-        coin : litecoin.data.coin,
-        ticker : litecoin.data.ticker,
-        prices: parseFloat(litecoin.data.prices.USD).toFixed(2)
+        logo: litecoin.data.logo,
+        coin: litecoin.data.coin,
+        ticker: litecoin.data.ticker,
+        prices: parseFloat(litecoin.data.prices.USD).toFixed(2),
       },
       {
-        logo : tron.data.logo,
-        coin : tron.data.coin,
-        ticker : tron.data.ticker,
-        prices: parseFloat(tron.data.prices.USD).toFixed(2)
-      }
-    ]
+        logo: tron.data.logo,
+        coin: tron.data.coin,
+        ticker: tron.data.ticker,
+        prices: parseFloat(tron.data.prices.USD).toFixed(2),
+      },
+    ];
 
     return res.send(apiCoin);
   } catch (error) {
